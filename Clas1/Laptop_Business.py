db8e2a0e311a137400244895f66617760628cb77
LuisLlumiquinga <lmllumiquinga3@espe.edu.ec>
2026-03-15 10:38:27 -0500

    Reto 8 OK

---
+++ Clas1/Laptop_Business.py
@@ -0,0 +1,29 @@
+from laptop import Laptop
+import random
+
+class Laptop_Business(Laptop):
+    def __init__(self, marca, procesador, memoria, espacio_disco, duracion_bateria, costo=500, impuesto=10):
+        super().__init__(marca, procesador, memoria, costo, impuesto)
+        self.espacio_disco = espacio_disco
+        self.duracion_bateria = duracion_bateria
+
+    def realizar_diagnostico_sistema(self):
+        resultado_diagnostico=super().realizar_diagnostico_sistema()
+        resultado_red = self.verificar_conectividad_red()
+       
+        resultado_diagnostico["ALMACENAMIENTO"] = "OK" if self.espacio_disco >= 256 else "Almacenamiento bajo"
+        resultado_diagnostico["BATERIA DURACION"] = "OK" if self.duracion_bateria >= 8 else "Bateria insuficiente para jornada laboral"
+        resultado_diagnostico["CONECTIVIDAD"] = resultado_red
+        
+        return resultado_diagnostico
+
+    def verificar_conectividad_red(self):
+        latencia = random.randint(1, 200)
+        resultado = {
+            "WIFI": random.choice([True, False]),
+            "SERVIDOR_EMPRESARIAL": random.choice([True, False]),
+            "LATENCIA":f"{latencia}ms-"+("OK" if latencia <= 50 else "Alta latencia")
+        }
+        return resultado
+
+    pass
--- Clas1/test_laptop.py
+++ Clas1/test_laptop.py
@@ -1,4 +1,6 @@
 from laptop import Laptop
+from laptop_gaming import Laptop_Gaming
+from Laptop_Business import Laptop_Business
 
 laptop_pepito = Laptop("lenovo", "i7", 32)
 laptop_maria = Laptop("lenovo", "i7", 32, 600)
@@ -8,6 +10,14 @@ laptop_maria = Laptop("lenovo", "i7", 32, 600)
 
 print(Laptop.comparar_costo(laptop_pepito, laptop_maria))
 
-for numero in range(1,1001):
-    asus_laptop = Laptop.asus_laptop(numero)
-    print(asus_laptop.__dict__)
+# for numero in range(1,1001):
+#     asus_laptop = Laptop.asus_laptop(numero)
+#     print(asus_laptop.__dict__)
+
+laptop_juanito = Laptop_Gaming("MSI", "I7", 4, "RTX 8GB")
+
+print(laptop_juanito.realizar_diagnostico_sistema())
+
+laptop_empresa = Laptop_Business("Dell", "i7", 16, 512, 10)
+print("\nLAPTOP EMPRESA")
+print(laptop_empresa.realizar_diagnostico_sistema())
